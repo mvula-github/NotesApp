7957072f7a4764fd4daac89b528915392cdb88e6
mvula <mvulakolweni08@gmail.com>
2025-05-16 03:14:41 +0200

    setup navigation bar components

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default Login;
