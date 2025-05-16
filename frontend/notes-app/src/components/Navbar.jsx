af1f6b67629e124ef20615f48d5cbbdbdac6c83e
mvula <mvulakolweni08@gmail.com>
2025-05-16 08:39:20 +0200

    modified profile card for simple desplay

--- frontend/notes-app/src/components/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ProfileInfo from "./ProfileInfo";
 
 const Navbar = () => {
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between px-6 py-6 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <ProfileInfo />
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
--- frontend/notes-app/src/utils/helper.js
+++ frontend/notes-app/src/utils/helper.js
@@ -2,3 +2,15 @@ export const validateEmail = (email) => {
   const regrex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regrex.test(email);
 };
+
+export const getInitials = () => {
+  if (!name) return "";
+
+  const words = name.split(" ");
+  let initials = "";
+
+  for (let i = 0; Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+  return initials.toUpperCase();
+};
