c96113e78cf3b05ef7afb2a514c5ee2fe7e8868b
mvula <mvulakolweni08@gmail.com>
2025-05-16 10:28:32 +0200

    added tile section, content, tags and add button for when user whats to add ther notes

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import NoteCard from "../../components/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <Navbar />
@@ -26,12 +32,22 @@ const Home = () => {
 
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-black hover:bg-blue-600 absolute right-10 bottom-10"
-        onClick={() => {}}
+        onClick={() => {
+          setOpenAddEditModal({ isShown: true, type: "add", data: null });
+        }}
       >
         <MdAdd className="text-[32px] text-white" />
       </button>
 
-      <AddEditNotes />
+      <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{ overlay: { backgroundColor: "rgba(0,0,0,0.2)" } }}
+        contentLabel=""
+        className=""
+      >
+        <AddEditNotes />
+      </Modal>
     </>
   );
 };
