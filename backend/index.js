7985aad1d6c0b5f4ebff6ba11bd803e49fa77468
mvula <mvulakolweni08@gmail.com>
2025-05-16 22:13:32 +0200

    created all crud points for notes

--- backend/index.js
+++ backend/index.js
@@ -131,5 +131,80 @@ app.post("/add-note", authenticateToken, async (req, res) => {
   }
 });
 
-app.listen(8000);
+//edit note
+app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { title, content, tags, isPinned } = req.body || {};
+  const { user } = req.user;
+
+  if (!title && !content && !tags) {
+    return res.status(400).json({ error: true, message: "No changes found" });
+  }
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    if (title) note.title = title;
+    if (content) note.content = content;
+    if (tags) note.tags = tags;
+    if (isPinned !== undefined) note.isPinned = isPinned;
+
+    await note.save();
+
+    return res.json({ error: false, message: "Note updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Something went wrong" });
+  }
+});
+
+//get all notes
+app.get("/get-all-notes", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+
+  try {
+    const notes = await Note.find({ userId: user._id }).sort({
+      createdAt: -1,
+    });
+
+    return res.json({
+      error: false,
+      notes,
+      message: "Notes retrieved successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Something went wrong" });
+  }
+});
+
+//delete note
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
+  const noteId = req.params.noteId;
+  const { user } = req.user;
+
+  try {
+    const note = await Note.findOne({ _id: noteId, userId: user._id });
+
+    if (!note) {
+      return res.status(404).json({ error: true, message: "Note not found" });
+    }
+
+    await Note.deleteOne({ _id: noteId });
+
+    return res.json({ error: false, message: "Note deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: true, message: "Something went wrong" });
+  }
+});
+
+app.listen(process.env.PORT);
 module.exports = app;
